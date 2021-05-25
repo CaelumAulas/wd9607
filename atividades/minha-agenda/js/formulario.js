a67a9cfb5e3e074a853bdb3b0cd5678994d11f33
Jhonatan Jacinto <jhonatanjacinto@gmail.com>
2021-05-24 22:48:49 -0300

    Aula 5

--- atividades/minha-agenda/js/formulario.js
+++ atividades/minha-agenda/js/formulario.js
@@ -1,1 +1,25 @@
+import { adicionarLinha } from "./tabelaContatos.js";
+
 // todo o código que tenha a ver com o gerenciamento do formulário
+const formulario = document.querySelector('#formCadastro');
+
+formulario.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const inputNome = formulario.querySelector('#inputNome');
+    const inputTel = formulario.querySelector('#inputTelefone');
+
+    if (inputNome.value.trim() == '') {
+        alert(inputNome.dataset.msg);
+    }
+    else if (inputTel.value.trim() == '') {
+        alert(inputTel.dataset.msg);
+    }
+    else {
+        // cadastrar o contato na tabela
+        let nome = inputNome.value;
+        let tel = inputTel.value;
+
+        adicionarLinha(nome, tel);
+        formulario.reset();
+    }
+});
--- atividades/minha-agenda/js/tabelaContatos.js
+++ atividades/minha-agenda/js/tabelaContatos.js
@@ -1,1 +1,24 @@
 // todo o código que tenha a ver com a tabela
+const tabela = document.querySelector('#tabelaContatos');
+
+export function adicionarLinha(nome, telefone) {
+    const tr = document.createElement('tr');
+
+    tr.innerHTML = `
+        <td>${nome}</td>
+        <td>${telefone}</td>
+        <td>
+            <button	class="btn btn-danger btn-sm">
+                Excluir
+            </button>
+        </td>
+    `;
+
+    tabela.append(tr);
+}
+
+tabela.addEventListener('click', function (event) {
+    if (event.target.classList.contains('btn-danger')) {
+        event.target.closest('tr').remove();
+    }
+});
